292b1c9773f4b2224250cdcdea4ef79a656a09c8
fatih kilic <fatih kilic>
2017-03-21 10:53:27 +0100

    This is my first commit!

---
+++ XamarinAlliance/XamarinAllianceApp/Characterinfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinAllianceApp.Models;
+
+namespace XamarinAllianceApp
+{
+  
+ class Characterinfo
+    {
+        public Character Cinfo { get; set; }
+        public Characterinfo(Character character = null)
+        {
+            Cinfo = character;
+            
+        }
+    }
+}
---
+++ XamarinAlliance/XamarinAllianceApp/Views/Page1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinAllianceApp.Controllers;
+using XamarinAllianceApp.Models;
+
+namespace XamarinAllianceApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+    public  Character Item;
+        public Page1(Character c)
+        {
+            InitializeComponent();
+           BindingContext =  this.Item = c;
+            Title = c.Name;
+            
+
+        }
+        async void OnPreviousPageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
